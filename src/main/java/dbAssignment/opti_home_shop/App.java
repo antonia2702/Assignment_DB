c878691c33bfdce39c0de56f105d6b0fdc6a8c1a
Antonia Dieterich <a.dieterich27@gmail.com>
2020-10-28 15:54:00 +0100

    initial commit

---
+++ src/main/java/dbAssignment/opti_home_shop/App.java
@@ -0,0 +1,13 @@
+package dbAssignment.opti_home_shop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
